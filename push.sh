aa4a29843bb305061ea1c64bd9149c8403e220d1
zmaplex <zmaplex@gmail.com>
2024-02-26 19:36:32 +0800

    updated

--- push.sh
+++ push.sh
@@ -1,2 +1,2 @@
 CGO_ENABLED=0  go build -v -trimpath -ldflags "-X 'github.com/sagernet/sing-box/constant.Version=unknown' -s -w -buildid=" -tags with_dhcp,with_quic,with_ech ./cmd/sing-box 
-aws s3 cp sing-box s3://$S3_SING_BOX_BUCKET --acl public-read
+aws s3 cp sing-box s3://$S3_SING_BOX_BUCKET --acl public-read --endpoint-url https://s3-accelerate.amazonaws.com
