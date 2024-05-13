f4607413f8204ab52cc70b881bcb06c675a2e994
zmaplex <zmaplex@gmail.com>
2024-05-13 14:18:07 +0800

    updated

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #  1469  git submodule update
 #  1470  git submodule foreach git checkout main
 #  1471  git submodule foreach git pull
-CGO_ENABLED=0 go build -v -trimpath -ldflags "-X 'github.com/sagernet/sing-box/constant.Version=1.8.13-pro.0' -s -w -buildid=" -tags with_dhcp,with_quic,with_ech ./cmd/sing-box
+CGO_ENABLED=0 go build -v -trimpath -ldflags "-X 'github.com/sagernet/sing-box/constant.Version=1.8.14-pro.0' -s -w -buildid=" -tags with_dhcp,with_quic,with_ech ./cmd/sing-box
 aws s3 cp sing-box s3://$S3_SING_BOX_BUCKET --acl public-read --endpoint-url https://s3-accelerate.amazonaws.com
