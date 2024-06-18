8335ee5e94f2ba16c71189d24044ed5710996e94
zmaplex <zmaplex@gmail.com>
2024-06-18 16:06:06 +0800

    updated

---
+++ unlock_test.sh
@@ -0,0 +1,16 @@
+response=$(curl --socks5 127.0.0.1:1080 -o /dev/null -s -w "%{http_code}\n" https://android.chat.openai.com)
+echo "$response https://android.chat.openai.com"
+if [ "$response" -ne 403 ]; then
+    echo "openai is locked"
+    exit 1
+fi
+echo "openai is unlocked"
+
+response=$(curl --socks5 127.0.0.1:1080 -o /dev/null -s -w "%{http_code}\n" https://www.netflix.com/sg-zh/title/70143836)
+
+echo "$response https://www.netflix.com/sg-zh/title/70143836"
+if [ "$response" -ne 200 ]; then
+    echo "netflix is locked"
+    exit 1
+fi
+echo "netflix is unlocked"
