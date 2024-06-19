bab7dd2abd95b774882321f41ef6a6d25d4f41bc
zmaplex <zmaplex@gmail.com>
2024-06-19 14:23:56 +0800

    updated

--- .github/workflows/unlock_test.sh
+++ .github/workflows/unlock_test.sh
@@ -1,5 +1,4 @@
 # 2404:c140
-curl -s https://android.chat.openai.com/cdn-cgi/trace -v --socks5 127.0.0.1:1080
 
 if ! curl -s https://android.chat.openai.com/cdn-cgi/trace --socks5 127.0.0.1:1080 | grep -Eq "2404:c140|38.150.8"; then
     echo "openai is locked"
